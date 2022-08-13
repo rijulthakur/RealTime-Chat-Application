d1df600642bd6a14138990a4f0d780e7b0c1e1cf
rijulthakur <rijul.thakur01@gmail.com>
2022-08-13 12:24:49 +0530

    check

---
+++ public/client.js
@@ -0,0 +1,54 @@
+const socket= io()
+let hello;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+do{
+    hello=prompt('Please enter your name: ')
+}while(!hello)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: hello,
+        message: message.trim()
+    }
+
+    appendMessage(msg, 'outgoing')
+
+    //sending to server
+    socket.emit('message', msg)
+
+
+    textarea.value = ''
+    scrollToBottom()
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    
+    mainDiv.innerHTML=markup
+    messageArea.appendChild(mainDiv)
+}
+
+
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom(){
+    messageArea.scrollTop= messageArea.scrollHeight
+}
