1c92113787d060ed4ce40ef57d6a172e92ec2ff4
Rijul <89183735+rijulthakur@users.noreply.github.com>
2022-09-06 10:28:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express= require('express')
 const app= express()
 const http= require('http').createServer(app)
 const PORT= process.env.PORT || 3000
-
 http.listen(PORT, ()=> {
     console.log(`Listening on port ${PORT}`)
 })
